b6bcdfde549948c0ab9b5b59e0b8df3432705577
Sudip1014 <mahtabhossain1014@gmail.com>
2023-11-24 21:42:26 +0600

    implement tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,19 @@ import PublicRoutes from "./routers/PublicRoutes";
 import AuthProvider from "./providers/AuthProvider";
 import { Toaster } from "react-hot-toast";
 import { HelmetProvider } from "react-helmet-async";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <AuthProvider>
-        <Toaster />
-        <RouterProvider router={PublicRoutes} />
-      </AuthProvider>
-    </HelmetProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <Toaster />
+          <RouterProvider router={PublicRoutes} />
+        </HelmetProvider>
+      </QueryClientProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
