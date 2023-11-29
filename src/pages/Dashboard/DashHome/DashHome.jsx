37464372532e9a9baf7c42c56daf327d1afad0cf
Sudip1014 <mahtabhossain1014@gmail.com>
2023-11-29 11:32:18 +0600

    fix style issues

--- src/pages/Dashboard/DashHome/DashHome.jsx
+++ src/pages/Dashboard/DashHome/DashHome.jsx
@@ -1,50 +1,77 @@
 import SpinnerLoader from "../../../components/PreLoader/SpinnerLoader";
+import useAdmin from "../../../hooks/useAdmin";
 import useTotalCount from "../../../hooks/useTotalCount";
 import moment from "moment";
 
 const DashHome = () => {
   const [totalCount, totalCountLoading] = useTotalCount();
+  const [isAdmin] = useAdmin();
 
-  if (totalCountLoading) {
+  if (isAdmin === "admin" && totalCountLoading) {
     return <SpinnerLoader />;
   }
 
   const date = moment().format("MMMM Do YYYY");
 
-
-  console.log(date);
-  
   return (
     <section>
-      <div className="flex justify-center py-10">
-        <div className="stats stats-vertical lg:stats-horizontal shadow">
-          <div className="stat place-items-center">
-            <div className="stat-title">Total USER</div>
-            <div className="stat-value text-secondary">
-              {totalCount?.total_user}
-            </div>
-            <div className="stat-desc text-secondary">↗︎ 40 (2%)</div>
-          </div>
-          <div className="stat place-items-center">
-            <div className="stat-title">Total Survey</div>
-            <div className="stat-value">{totalCount?.total_survey}</div>
-            <div className="stat-desc">From Start to {date}</div>
-          </div>
-          <div className="stat place-items-center">
-            <div className="stat-title">Total Comments</div>
-            <div className="stat-value">{totalCount?.total_comment}</div>
-            <div className="stat-desc">From Start to {date}</div>
-          </div>
+      {isAdmin === "admin" ? (
+        <>
+          <div className="flex justify-center py-10">
+            <div className="stats stats-vertical lg:stats-horizontal shadow">
+              <div className="stat place-items-center">
+                <div className="stat-title">Total USER</div>
+                <div className="stat-value text-secondary">
+                  {totalCount?.total_user}
+                </div>
+                <div className="stat-desc text-secondary">↗︎ 40 (2%)</div>
+              </div>
+              <div className="stat place-items-center">
+                <div className="stat-title">Total Survey</div>
+                <div className="stat-value">{totalCount?.total_survey}</div>
+                <div className="stat-desc">From Start to {date}</div>
+              </div>
+              <div className="stat place-items-center">
+                <div className="stat-title">Total Comments</div>
+                <div className="stat-value">{totalCount?.total_comment}</div>
+                <div className="stat-desc">From Start to {date}</div>
+              </div>
 
-          <div className="stat place-items-center">
-            <div className="stat-title">Total Revenue</div>
-            <div className="stat-value">
-              ${totalCount?.total_revenue[0]?.total}
+              <div className="stat place-items-center">
+                <div className="stat-title">Total Revenue</div>
+                <div className="stat-value">
+                  ${totalCount?.total_revenue[0]?.total}
+                </div>
+                <div className="stat-desc">↘︎ 90 (14%)</div>
+              </div>
             </div>
-            <div className="stat-desc">↘︎ 90 (14%)</div>
           </div>
-        </div>
-      </div>
+        </>
+      ) : (
+        <>
+          <div className="mt-5 md:mt-0 md:p-5">
+            <div className="mockup-window border bg-base-300">
+              <div className="flex flex-col items-center px-4 py-16 bg-base-200">
+                <h2 className="text-3xl">Sorry To Say</h2>
+                <p className="text-lg mb-5">Here is your dashboard</p>
+                <div className="diff aspect-[16/4]">
+                  <div className="diff-item-1">
+                    <div className="bg-primary text-primary-content text-3xl md:text-7xl font-black grid place-content-center">
+                      DASHBOARD
+                    </div>
+                  </div>
+                  <div className="diff-item-2">
+                    <div className="bg-base-200 text-3xl md:text-7xl font-black grid place-content-center">
+                      DASHBOARD
+                    </div>
+                  </div>
+                  <div className="diff-resizer"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </section>
   );
 };
